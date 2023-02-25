14454de2bf8bc36f1594fb030df3041e842f7bf9
Jairo Mora <jairomora61@gmail.com>
2023-02-25 18:55:16 +0100

    initial commit

---
+++ app/src/main/java/com/example/actividad_331_jairo_mora_enriquez/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.actividad_331_jairo_mora_enriquez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
